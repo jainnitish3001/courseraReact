2225392a6bf1bae2c308963b486da398c67e2ccf
duttrohan0302 <duttrohan0302@gmail.com>
2020-06-21 00:18:30 +0530

    controlled forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { PROMOTIONS } from './../shared/promotions'
 import { LEADERS } from './../shared/leaders'
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
   constructor(props){
@@ -48,6 +49,7 @@ class Main extends Component {
           <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes} /> } />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route path='/contactus' component={Contact} />
+          <Route path='/aboutus' component={()=><About leaders={this.state.leaders} />} />
           <Redirect to='/home' />
         </Switch>
         <Footer />
